468a7355f64c45209be34e769eaacb876415df16
lbp <lbp@liuboping.com>
2017-10-31 00:00:19 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,21 @@
                                     </small>
                                 </footer>
                             </blockquote>
+                            <v-list two-line>
+                                <template v-for="item in items">
+                                    <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+                                    <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+                                    <v-list-tile avatar v-else v-bind:key="item.title" @click="">
+                                        <v-list-tile-avatar>
+                                            <img v-bind:src="item.avatar">
+                                        </v-list-tile-avatar>
+                                        <v-list-tile-content>
+                                            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                    </v-list-tile>
+                                </template>
+                            </v-list>
                             <template>
                                 <v-expansion-panel focusable>
                                     <v-expansion-panel-content v-for="(mirror,i) in mirrors" :key="i">
@@ -26,7 +41,7 @@
                                         <v-card>
                                             <v-card-text class="grey lighten-3">
                                                 <a :href="mirror.uri" rel="nofollow">{{ mirror.name }}</a>
-                                                <a :href="mirror.help">help</a>
+                                                <a v-if="mirror.help" :href="mirror.help">help</a>
                                                 <br>
                                                 {{ mirror.content }}
                                             </v-card-text>
@@ -56,9 +71,9 @@
                 right: true,
                 rightDrawer: false,
                 title: '萝卜皮开源软件镜像',
-                mirrors: [
-                    {name: 'nginx', uri: '/nginx/', help: '', content: 'nginx'},
-                    {name: 'ubuntu', uri: '/ubuntu/', help: '', content: 'nginx'},
+                items: [
+                    {header: 'Nginx', uri: '/nginx/', help: '', content: 'nginx'},
+                    {header: 'Ubuntu', uri: '/ubuntu/', help: '', content: 'nginx'},
                 ]
             }
         }
